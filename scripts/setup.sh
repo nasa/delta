422cb26917c2b8bc8e28d8d3d04e000f5b45910e
Brian Coltin <brian.j.coltin@nasa.gov>
2019-08-12 09:33:46 -0700

    Install newer version of gdal in setup.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# use this repository and key for gdal-2.0
+sudo add-apt-repository ppa:ubuntugis/ppa
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 089EBE08314DF160
+sudo apt update
+
 sudo apt install -y python3-pip python3-dev python3-gdal || { echo >&2 "ERROR. Failed to install pip3."; exit 1; }
 
 pip3 install --user -q pylint psutil usgs || { echo >&2 "ERROR. Failed to install prequired packages."; exit 1; }
