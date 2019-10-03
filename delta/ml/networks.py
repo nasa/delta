2e00ebae407361a07b68dd41fc90355e38847428
P. Michael Furlong <padraig.m.furlong@nasa.gov>
2019-10-03 13:32:07 -0700

    Added task specific networks, removed sigmoid output from autoencoder

--- delta/ml/networks.py
+++ delta/ml/networks.py
@@ -16,10 +16,46 @@ def make_autoencoder(in_shape, encoding_size=32):
     model = keras.Sequential([
         keras.layers.Flatten(input_shape=in_shape),
         keras.layers.Dense(encoding_size, activation=tf.nn.relu),
-        keras.layers.Dense(np.prod(in_shape), activation=tf.nn.sigmoid),
+        keras.layers.Dense(np.prod(in_shape), activation=tf.nn.relu),
         keras.layers.Reshape(in_shape)
         ])
     print(model.summary())
     return model
 
-### end make_model
+### end make_autoencoder
+
+def make_task_specific(in_shape, out_shape):
+    '''
+    Constructs an arbitrarily sized dense neural network.
+    TODO: Come up with a more principled network selection method.
+    '''
+    fc1_size = np.prod(in_shape)
+    # There is no reason behind any of the following values.
+    fc2_size = 253
+    fc3_size = 253
+    fc4_size = 81
+
+    dropout_rate = 0.3
+
+    mlflow.log_param('fc1_size',fc1_size)
+    mlflow.log_param('fc2_size',fc2_size)
+    mlflow.log_param('fc3_size',fc3_size)
+    mlflow.log_param('fc4_size',fc4_size)
+    mlflow.log_param('dropout_rate',dropout_rate)
+
+
+    # Define network
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=in_shape),
+        keras.layers.Dense(fc2_size, activation=tf.nn.relu),
+        keras.layers.Dropout(rate=dropout_rate),
+        keras.layers.Dense(fc3_size, activation=tf.nn.relu),
+        keras.layers.Dropout(rate=dropout_rate),
+        keras.layers.Dense(fc4_size, activation=tf.nn.relu),
+        keras.layers.Dropout(rate=dropout_rate),
+        keras.layers.Dense(out_shape, activation=tf.nn.sigmoid),
+        keras.layers.Softmax()
+        ])
+    return model
+
+### end make_task_specific
