ddac1f991cf877f57f5c2353874b052b505310cb
Michael Furlong <padraig.m.furlong@nasa.gov>
2019-10-30 10:21:16 -0700

    Modified the tensorflow and python version checks to report what verion is visible to the python environment.

--- delta/__init__.py
+++ delta/__init__.py
@@ -7,9 +7,9 @@ import sys
 import tensorflow
 
 if sys.version_info < (3, 0, 0):
-    print('\nERROR: DELTA code requires Python version >= 3.0.')
+    print('\nERROR: DELTA code requires Python version >= 3.0.  Installed is %s' % (sys.version_info,))
     sys.exit(1)
 
 if re.search('^2.0', tensorflow.__version__) is None:
-    print('\nERROR: DELTA code requires Tensorflow version 2.0')
+    print('\nERROR: DELTA code requires Tensorflow version 2.0 installed: %s' %(tensorflow.__version__, ))
     sys.exit(1)
