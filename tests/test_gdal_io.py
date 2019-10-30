f0f9ea3008fe24ba166858e0350e4a683de15c71
Michael Furlong <padraig.m.furlong@nasa.gov>
2019-10-30 14:29:19 -0700

    Made test_gdal_io.py pylint consistent

--- tests/test_gdal_io.py
+++ tests/test_gdal_io.py
@@ -11,6 +11,9 @@ from delta.imagery.image_reader import MultiTiffFileReader
 from delta.imagery.image_writer import TiffWriter
 
 def check_landsat_tiff(filename):
+    '''
+    Checks reading landsat tiffs.
+    '''
     input_reader = MultiTiffFileReader()
     input_reader.load_images([filename])
     assert input_reader.image_size() == (37, 37)
@@ -26,21 +29,26 @@ def check_landsat_tiff(filename):
     meta = input_reader.get_all_metadata()
     geo = meta['geotransform']
     assert geo[0] == pytest.approx(-122.3, abs=0.01)
-    assert geo[1] == pytest.approx(0.0,    abs=0.01)
-    assert geo[2] == pytest.approx(0.0,    abs=0.01)
-    assert geo[3] == pytest.approx(37.5,   abs=0.01)
-    assert geo[4] == pytest.approx(0.0,    abs=0.01)
-    assert geo[5] == pytest.approx(0.0,    abs=0.01)
+    assert geo[1] == pytest.approx(0.0, abs=0.01)
+    assert geo[2] == pytest.approx(0.0, abs=0.01)
+    assert geo[3] == pytest.approx(37.5, abs=0.01)
+    assert geo[4] == pytest.approx(0.0, abs=0.01)
+    assert geo[5] == pytest.approx(0.0, abs=0.01)
     assert 'gcps' in meta
     assert 'gcpproj' in meta
     assert 'projection' in meta
     assert 'metadata' in meta
 
-    r = rectangle.Rectangle(0, 0, width=input_reader.image_size()[0], height=input_reader.image_size()[0])
-    d1 = input_reader.parallel_load_chunks(r, 1, 0)
-    assert d1.shape == (input_reader.image_size()[0] * input_reader.image_size()[1], input_reader.num_bands(), 1, 1)
+    rect = rectangle.Rectangle(0, 0, width=input_reader.image_size()[0],
+                               height=input_reader.image_size()[0])
+    d_1 = input_reader.parallel_load_chunks(rect, 1, 0)
+    assert d_1.shape == (input_reader.image_size()[0] * input_reader.image_size()[1],
+                         input_reader.num_bands(), 1, 1)
 
 def check_same(filename1, filename2):
+    '''
+    Checks whether or not two files are the same
+    '''
     in1 = MultiTiffFileReader()
     in2 = MultiTiffFileReader()
     in1.load_images([filename1])
@@ -52,23 +60,29 @@ def check_same(filename1, filename2):
         assert in1.data_type(i) == in2.data_type(i)
         assert in1.nodata_value(i) == in2.nodata_value(i)
 
-    m1 = in1.get_all_metadata()
-    m2 = in2.get_all_metadata()
-    assert m1['geotransform'] == m2['geotransform']
-    assert m1['gcps'] == m2['gcps']
-    assert m1['gcpproj'] == m2['gcpproj']
-    assert m1['projection'] == m2['projection']
-    assert m1['metadata'] == m2['metadata']
+    m_1 = in1.get_all_metadata()
+    m_2 = in2.get_all_metadata()
+    assert m_1['geotransform'] == m_2['geotransform']
+    assert m_1['gcps'] == m_2['gcps']
+    assert m_1['gcpproj'] == m_2['gcpproj']
+    assert m_1['projection'] == m_2['projection']
+    assert m_1['metadata'] == m_2['metadata']
 
     (width, height) = in1.image_size()
-    d1 = in1.parallel_load_chunks(rectangle.Rectangle(0, 0, width=width, height=height), 1, 0)
-    d2 = in2.parallel_load_chunks(rectangle.Rectangle(0, 0, width=width, height=height), 1, 0)
-    assert np.array_equal(d1, d2)
+    d_1 = in1.parallel_load_chunks(rectangle.Rectangle(0, 0, width=width, height=height), 1, 0)
+    d_2 = in2.parallel_load_chunks(rectangle.Rectangle(0, 0, width=width, height=height), 1, 0)
+    assert np.array_equal(d_1, d_2)
 
 def test_geotiff_read():
+    '''
+    Tests reading a landsat geotiff.
+    '''
     check_landsat_tiff('data/landsat.tiff')
 
 def test_geotiff_write(tmpdir):
+    '''
+    Tests writing a landsat geotiff.
+    '''
     input_reader = MultiTiffFileReader()
     input_reader.load_images(['data/landsat.tiff'])
     new_tiff = tmpdir / 'test.tiff'
@@ -86,11 +100,11 @@ def test_geotiff_write(tmpdir):
 
     input_bounds = rectangle.Rectangle(0, 0, width=cols, height=rows)
     output_rois = []
-    for r in range(0, blocks_y):
-        for c in range(0, blocks_x):
+    for row in range(0, blocks_y):
+        for col in range(0, blocks_x):
 
             # Get the ROI for the block, cropped to fit the image size.
-            roi = rectangle.Rectangle(c * block_size[0], r * block_size[1],
+            roi = rectangle.Rectangle(col * block_size[0], row * block_size[1],
                                       width=block_size[0], height=block_size[1])
             roi = roi.get_intersection(input_bounds)
             output_rois.append(roi)
@@ -103,17 +117,17 @@ def test_geotiff_write(tmpdir):
         row = output_roi.min_y // block_size[1]
 
         # Figure out where the desired output data falls in read_roi
-        x0 = output_roi.min_x - read_roi.min_x
-        y0 = output_roi.min_y - read_roi.min_y
-        x1 = x0 + output_roi.width()
-        y1 = y0 + output_roi.height()
-        assert x0 == 0
-        assert y0 == 0
+        x_0 = output_roi.min_x - read_roi.min_x
+        y_0 = output_roi.min_y - read_roi.min_y
+        x_1 = x_0 + output_roi.width()
+        y_1 = y_0 + output_roi.height()
+        assert x_0 == 0
+        assert y_0 == 0
 
         # Crop the desired data portion and write it out.
         for i in range(input_reader.num_bands()):
-            output_data = data_vec[i][y0:y1, x0:x1]
-            assert data_vec[i].shape == (y1 - y0, x1 - x0)
+            output_data = data_vec[i][y_0:y_1, x_0:x_1]
+            assert data_vec[i].shape == (y_1 - y_0, x_1 - x_0)
             writer.write_geotiff_block(output_data, col, row, band=i)
 
     input_reader.process_rois(output_rois, callback_function)
