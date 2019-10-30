21b3dbd8e70b0524bb6b9564378ac89038884b2c
Michael Furlong <padraig.m.furlong@nasa.gov>
2019-10-30 14:21:07 -0700

    Effors to make the pylint gods happy

--- tests/test_autoencoder.py
+++ tests/test_autoencoder.py
@@ -1,18 +1,29 @@
-from delta.ml import networks, train
-import tensorflow as tf
-from tensorflow import keras
+'''
+file: test_autoencoder.py -- Runs the autoencoder from the delta project through some
+        simple unit tests.
+date: 2019.10.29
+'''
+
 import functools
 import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+from delta.ml import networks, train
 
 MNIST_WIDTH = 28 # The images are 28x28 pixels, single channel
 MNIST_BANDS = 1
 MNIST_MAX = 255.0 # Input images are 0-255
 
 def dataset_fashion_mnist(batch_size=200, num_epochs=5, shuffle_buffer_size=10):
+    '''
+    Creates a Tensorflow dataset out of the MNIST fashion dataset for training an
+    autoencode
+    '''
     fashion_mnist = keras.datasets.fashion_mnist
-    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    (train_images, _), (_, _) = fashion_mnist.load_data()
     train_images = train_images / MNIST_MAX
-    (num_data, rows, cols) = train_images.shape
+    (num_data, _, _) = train_images.shape
     train_images = np.reshape(train_images, (num_data, MNIST_WIDTH, MNIST_WIDTH, MNIST_BANDS))
 
     d_s = tf.data.Dataset.zip((tf.data.Dataset.from_tensor_slices(train_images),
@@ -22,24 +33,31 @@ def dataset_fashion_mnist(batch_size=200, num_epochs=5, shuffle_buffer_size=10):
 
 
 def test_dense_autoencoder():
+    '''
+    tests the performance of the dense autoencoder on the Fashion MNIST dataset
+    '''
 
     num_epochs = 1
     in_shape = (MNIST_WIDTH, MNIST_WIDTH, MNIST_BANDS)
-    model_fn = functools.partial(networks.make_autoencoder, 
+    model_fn = functools.partial(networks.make_autoencoder,
                                  in_shape, encoding_size=32, encoder_type='dense')
     data_fn = functools.partial(dataset_fashion_mnist, num_epochs=num_epochs)
-   
-    model, history = train.train(model_fn, dataset_fashion_mnist, num_epochs=num_epochs)
+
+    model, history = train.train(model_fn, data_fn, num_epochs=num_epochs)
+    assert model is not None
     assert history.history['loss'][-1] < 0.04, "Terminal Loss was greater than 0.04"
 
 def test_conv_autoencoder():
+    '''
+    tests the performance of the convolutional autoencoder on the Fashion MNIST dataset
+    '''
 
     num_epochs = 1
     in_shape = (MNIST_WIDTH, MNIST_WIDTH, MNIST_BANDS)
-    model_fn = functools.partial(networks.make_autoencoder, 
+    model_fn = functools.partial(networks.make_autoencoder,
                                  in_shape, encoding_size=32, encoder_type='conv')
     data_fn = functools.partial(dataset_fashion_mnist, num_epochs=num_epochs)
-   
-    model, history = train.train(model_fn, dataset_fashion_mnist, num_epochs=num_epochs)
-    assert history.history['loss'][-1] < 0.04, "Terminal Loss was greater than 0.04"
 
+    model, history = train.train(model_fn, data_fn, num_epochs=num_epochs)
+    assert model is not None
+    assert history.history['loss'][-1] < 0.04, "Terminal Loss was greater than 0.04"
