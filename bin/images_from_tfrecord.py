29bee0fc8bcdc5cfeba5291a579b8915578a0ac0
Scott <scott.t.mcmichael@nasa.gov>
2019-12-04 11:25:05 -0800

    images_from_tfrecord can't used tfrecord.size()

--- bin/images_from_tfrecord.py
+++ bin/images_from_tfrecord.py
@@ -13,16 +13,15 @@ from delta.imagery.sources import tfrecord
 #------------------------------------------------------------------------------
 
 
-def images_from_tfrecord(input_path, output_prefix, width=4, compressed=True, label=False):
+def images_from_tfrecord(input_path, output_prefix, width_cap, compressed=True, label=False):
     """Extract entries from a tfrecord file and write them as plain image files"""
 
     # Get size information from the file
     image = tfrecord.TFRecordImage(input_path, compressed)
     num_bands = image.num_bands()
 
-    (input_region_height, input_region_width) = image.size()
-
-    dataset = tfrecord.create_dataset([input_path], num_bands, tf.uint8 if label else tf.float32)
+    dataset = tfrecord.create_dataset([input_path], num_bands, tf.uint8 if label else tf.float32,
+                                      compressed=compressed)
     iterator = iter(dataset)
 
     scaling = 1
@@ -30,14 +29,22 @@ def images_from_tfrecord(input_path, output_prefix, width=4, compressed=True, la
         scaling = 80
 
     # Read images until we hit an exception (end of file) then write out what we have
+    full_tile_width = None
     total  = 0
     count  = 0
     concat = None
     while True:
         try:
             image = next(iterator)
-            pic = image[:,:,0] # Just take the first channel for now
-            pic = tf.reshape(pic, ( input_region_height, input_region_width, 1))
+
+            # Get the size of this particulary tile
+            (height, width, _) = image.shape
+            if not full_tile_width:
+                full_tile_width = width
+
+            # Just take the first channel for now
+            pic = image[:,:,0]
+            pic = tf.reshape(pic, ( height, width, 1))
 
             pic = tf.cast(pic*scaling, tf.uint8)
 
@@ -48,8 +55,9 @@ def images_from_tfrecord(input_path, output_prefix, width=4, compressed=True, la
                 concat = pic
 
             print('Count, total = ' + str((count, total)))
-            # When we hit the desired width write out the data to an image file.
-            if count == width:
+            # Write out the concatenated image when we hit a narrow (end) tile or get too big
+            # - This strategy works well for individual images but not for mixed images.
+            if (width != full_tile_width) or (count >= width_cap):
                 jpeg = tf.image.encode_jpeg(concat, quality=100, format='grayscale')
                 output_path = output_prefix + str(total) + '.jpg'
                 tf.io.write_file(output_path, jpeg)
@@ -57,7 +65,7 @@ def images_from_tfrecord(input_path, output_prefix, width=4, compressed=True, la
                 concat = None
             count += 1
             total += 1
-        except OutOfRangeError:
+        except (OutOfRangeError, StopIteration):
             if count > 0:
                 jpeg = tf.image.encode_jpeg(concat, quality=100, format='grayscale')
                 output_path = output_prefix + str(total) + '.jpg'
@@ -84,8 +92,8 @@ def main(argsIn):
         parser.add_argument("--label", action="store_true", dest="label", default=False,
                             help="The input file is a label file.")
 
-        parser.add_argument("--width", dest="width", type=int, default=8,
-                            help="Default number of tiles to put in each output file.")
+        parser.add_argument("--width-cap", dest="width_cap", type=int, default=20,
+                            help="Cap the number of tiles in a row at this amount.")
 
         options = parser.parse_args(argsIn)
 
@@ -98,7 +106,7 @@ def main(argsIn):
     #    os.mkdir(options.output_folder)
 
     images_from_tfrecord(options.input_path, options.output_prefix,
-                         options.width, not options.uncompressed, options.label)
+                         options.width_cap, not options.uncompressed, options.label)
 
 
     print('Finished!')
