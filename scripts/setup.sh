9d0a57688437ce29e16e74e9ae4cd61d9c8064ff
Brian Coltin <brian.coltin@nasa.gov>
2019-12-16 14:59:00 -0800

    Clean up documentation.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,24 +9,6 @@ sudo apt update
 
 sudo apt install -y python3-pip python3-dev python3-gdal || { echo >&2 "ERROR. Failed to install pip3."; exit 1; }
 
-pip3 install --user -q pylint psutil usgs pytest || { echo >&2 "ERROR. Failed to install prequired packages."; exit 1; }
-pip3 install --user -q numpy scipy matplotlib || { echo >&2 "ERROR. Failed to install one of numpy, scipy, matplotlib."; exit 1; }
-
-pip3 install --user GDAL==$(gdal-config --version | awk -F'[.]' '{print $1"."$2}') --global-option=build_ext --global-option="-I/usr/include/gdal"/
-
-pip3 install --user portalocker
-
-pip3 install --user -q tensorflow || { echo >&2 "ERROR. Failed to install tensorflow."; exit 1; }
-
-# To install gpu tensorflow uncomment this line
-# pip3 install --user -q tensorflow-gpu || { echo >&2 "ERROR. Failed to install tensorflow."; exit 1; }
-
-pip3 install --user -q mlflow || { echo >&2 "ERROR. Failed to install mlflow."; exit 1; }
-pip3 install --user -q portalocker || { echo >&2 "ERROR. Failed to install mlflow."; exit 1; }
-
-
-#command -v pip3>/dev/null 2>&1 || { echo >&2 "ERROR. Please install pip3."; exit 1; }
-
 $DIR/linter/install_linter.sh || { echo >&2 "ERROR. Failed to install linter."; exit 1; }
 
 echo "All dependencies successfully installed."
