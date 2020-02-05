3ebf38f85f1fd9a819473efcacfca7b86e3a2d9c
Michael Furlong <padraig.m.furlong@nasa.gov>
2020-02-05 13:53:45 -0800

    Gave up on being clever and wrote the code to reshape predicted data into tiles

--- delta/ml/predict.py
+++ delta/ml/predict.py
@@ -4,6 +4,10 @@ import tensorflow as tf
 
 from delta.imagery import rectangle
 
+#pylint: disable=unsubscriptable-object
+# Pylint was barfing lines 32 and 76. See relevant bug report
+# https://github.com/PyCQA/pylint/issues/1498
+
 def predict_array(model, data):
 
     net_input_shape = model.get_input_shape_at(0)[1:]
@@ -21,23 +25,31 @@ def predict_array(model, data):
                                       [1, net_output_shape[0], net_output_shape[1], 1],
                                       [1, 1, 1, 1], padding='VALID')
     chunks = tf.reshape(chunks, (-1,) + net_input_shape)
+
     # TODO: Figure out correct shape for best for AE.
     best = np.zeros((chunks.shape[0],) + net_output_shape, dtype=np.float32)
     ## TODO: other data types, configurable batch size
     BATCH_SIZE=1000
     for i in range(0, chunks.shape[0], BATCH_SIZE):
+        # batches = model.predict_on_batch(chunks[i:i+BATCH_SIZE])
         best[i:i+BATCH_SIZE] = model.predict_on_batch(chunks[i:i+BATCH_SIZE])
 
-    return np.reshape(best, out_shape + (net_output_shape[-1],))
+    retval = np.zeros(out_shape + (net_output_shape[-1],))
+    for chunk_idx in range(0, best.shape[0]):
+        r = (chunk_idx // (  data.shape[1] // net_output_shape[1])) * net_output_shape[0]
+        c = (chunk_idx  % ( data.shape[1] // net_output_shape[1])) * net_output_shape[1]
+        retval[r:r+net_output_shape[0],c:c+net_output_shape[1],:] = best[chunk_idx,:,:,:]
+
+    return retval
 
 def predict_validate(model, image, label, input_bounds=None, probabilities=False, show_progress=False):
     """Like predict but returns (predicted image, error image, percent correct)."""
-    block_size_x = 256
-    block_size_y = 256
     net_input_shape = model.get_input_shape_at(0)[1:]
     net_output_shape = model.get_output_shape_at(0)[1:]
     offset_r = - net_input_shape[0] + net_output_shape[0]
     offset_c = - net_input_shape[1] + net_output_shape[1]
+    block_size_x = net_input_shape[0] * (256 // net_input_shape[0])
+    block_size_y = net_input_shape[1] * (256 // net_input_shape[1])
 
     # Set up the output image
     if not input_bounds:
@@ -82,11 +94,12 @@ def predict_validate(model, image, label, input_bounds=None, probabilities=False
 
     # TODO think about the below to test.
     output_rois = input_bounds.make_tile_rois(block_size_x - offset_r, block_size_y - offset_c,
-                                              include_partials=True, overlap_amount=-offset_r)
+                                              include_partials=False, overlap_amount=-offset_r)
 
     image.process_rois(output_rois, callback_function, show_progress=show_progress)
     return (result, errors, confusion_matrix)
 
+# TODO: Save data as we progress through it
 def predict(model, cs, image, num_classes, input_bounds=None, probabilities=False, show_progress=False):
     """Returns the predicted image given a model, chunk size, and image."""
     type(cs)
