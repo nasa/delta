bc22e879f770a587fc4489261a89515ecfa1e008
Michael Furlong <padraig.m.furlong@nasa.gov>
2020-02-10 10:12:17 -0800

    Removed redundant code.  Changed encoding size.  TODO: use model loader to load autoencoders

--- delta/subcommands/autoencode.py
+++ delta/subcommands/autoencode.py
@@ -1,11 +1,8 @@
 """
 Autoencoder.
 """
-import os
 
 import functools
-import matplotlib.pyplot as plt
-import numpy as np
 
 import mlflow
 import tensorflow as tf
@@ -15,16 +12,6 @@ from delta.imagery import imagery_dataset
 from delta.ml.train import train
 from delta.ml.networks import make_autoencoder
 
-
-# TODO: Move this function!
-def get_debug_bands(image_type):
-    '''Pick the best bands to use for debug images'''
-    bands = [0]
-    if image_type == 'worldview':
-        # TODO: Distinguish between WV2 and WV3
-        bands = [4,2,1] # RGB
-    # TODO: Support more sensors
-    return bands
 
 # With TF 1.12, the dataset needs to be constructed inside a function passed in to
 # the estimator "train_and_evaluate" function to avoid getting a graph error!
@@ -81,60 +68,16 @@ def main(options):
     # in the scope of the distribution strategy (occurrs in the training function)
     model_fn = functools.partial(make_autoencoder,
                                  data_shape,
-                                 #encoding_size=int(config.num_hidden())
-                                 encoding_size=32
+                                 encoding_size=200,
+                                 encoder_type='dense',
                                  )
 
-    #model, _ = train(model_fn, assemble_dataset(), config.training(), config.experiment_name())
     model, _ = train(model_fn, aeds, tc)
 
 
     print('Saving Model')
-    out_filename = os.path.join(config.output_folder(), options.model)
+    out_filename = options.model
+    #out_filename = os.path.join(config.output_folder(), options.model)
     tf.keras.models.save_model(model, options.model, overwrite=True, include_optimizer = True)
     mlflow.log_artifact(out_filename)
-
-    # Write input/output image pairs to the current working folder.
-    print('Recording ', str(options.num_debug_images), ' demo images.')
-
-
-    # Make a non-shuffled dataset with a simple iterator
-    ds = assemble_dataset_for_predict()
-    iterator = iter(ds)
-
-    #scale = aeds.scale_factor()
-    scale = 1.0
-    num_bands = data_shape[-1]
-#     debug_bands = get_debug_bands(config_values['input_dataset']['image_type'])
-#     print('debug_bands = ' + str(debug_bands))
-    for i in range(0, options.num_debug_images):
-        print('Preparing debug image ' + str(i))
-
-        value = next(iterator)
-        element = model.predict(value)
-
-        # Get the output value out of its weird format, then convert for image output
-        pic = (element['reshape'][:, :, :] * scale).astype(np.uint8)
-        pic = np.moveaxis(pic, 0, -1)
-
-        in_pic = (value[0][0,:,:,:] * scale).astype(np.uint8)
-        in_pic = np.moveaxis(in_pic, 0, -1)
-
-        for band in range(num_bands):
-            plt.subplot(num_bands,2,2*band+1)
-            plt.imshow(in_pic[:,:,band])
-            if band == 0:
-                plt.title('Input image %03d' % (i, ))
-
-            plt.subplot(num_bands,2,2*band+2)
-            plt.imshow(pic[:,:,band])
-            if band == 0:
-                plt.title('Output image %03d' % (i, ))
-
-        debug_image_filename = os.path.join(config.output_folder(),
-                                            'Autoencoder_input_output_%03d.png' % (i, ))
-        plt.savefig(debug_image_filename)
-
-        mlflow.log_artifact(debug_image_filename)
-
     return 0
