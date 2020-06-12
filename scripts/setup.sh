eed8a03f53c9ff734cf4ec05e6f80b30fe535d0b
Brian Coltin <brian.coltin@nasa.gov>
2020-06-12 15:12:59 -0700

    Add apt update.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # use this repository and key for gdal-2.0
 #sudo add-apt-repository -y ppa:ubuntugis/ppa
 #sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 089EBE08314DF160
-#sudo apt update
+sudo apt update
 
 #sudo apt install -y python3-gdal || { echo >&2 "ERROR. Failed to install python3."; exit 1; }
 sudo apt install -y python3-dev libgdal-dev || { echo >&2 "ERROR. Failed to install python3."; exit 1; }
