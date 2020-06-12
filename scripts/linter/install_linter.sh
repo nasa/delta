8caffe8304b8b57a14f39555311c4df9d0face2c
Brian Coltin <brian.coltin@nasa.gov>
2020-06-12 15:50:57 -0700

    Github Continuous Integration (#2)
    * Add github actions for linter and unit testing.

--- scripts/linter/install_linter.sh
+++ scripts/linter/install_linter.sh
@@ -2,6 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+python3 -m pip install pylint
+
 cp $DIR/pre-commit $DIR/../../.git/hooks/pre-commit
 echo "Linter installed as pre-commit hook."
 
