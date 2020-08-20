f09b314f952151bf3dd3b1b8cbf81b4ca7880f7a
Brian Coltin <brian.coltin@nasa.gov>
2020-08-20 11:59:57 -0700

    Process relative paths correctly in docs script.

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -2,6 +2,6 @@
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-OUT_DIR=${1:-./html}
+OUT_DIR=$(readlink -m ${1:-./html})
 cd $SCRIPTPATH/..
 pdoc3 --html -c show_type_annotations=True delta --force -o $OUT_DIR
