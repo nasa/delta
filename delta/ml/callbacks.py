88df9e2fe176d9e7a32debac9174afbb045ef5d0
Brian Coltin <brian.coltin@nasa.gov>
2020-10-09 14:01:19 -0700

    Learning rate scheduler callback and toggle trainable callback.

--- delta/ml/callbacks.py
+++ delta/ml/callbacks.py
@@ -15,12 +15,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
 from typing import List
 
 import tensorflow
 import tensorflow.keras.callbacks
 
 from delta.config import config
+
+class SetTrainable(tensorflow.keras.callbacks.Callback):
+    def __init__(self, layer_name, epoch, make_trainable=True):
+        super().__init__()
+        self._layer_name = layer_name
+        self._epoch = epoch
+        self._make_trainable = make_trainable
+
+    def on_epoch_begin(self, epoch, logs=None):
+        if epoch == self._epoch:
+            self.model.get_layer(self._layer_name).trainable = self._make_trainable
+
+def ExponentialLRScheduler(start_epoch=10, multiplier=0.95):
+    def schedule(epoch, lr):
+        if epoch < start_epoch:
+            return lr
+        return multiplier * lr
+    return tensorflow.keras.callbacks.LearningRateScheduler(schedule)
 
 def callback_from_dict(callback_dict) -> tensorflow.keras.callbacks.Callback:
     '''
@@ -29,7 +48,11 @@ def callback_from_dict(callback_dict) -> tensorflow.keras.callbacks.Callback:
     assert len(callback_dict.keys()) == 1, f'Error: Callback has more than one type {callback_dict.keys()}'
 
     layer_type = next(iter(callback_dict.keys()))
-    callback_class = getattr(tensorflow.keras.callbacks, layer_type, None)
+    callback_class = getattr(sys.modules[__name__], layer_type, None)
+    if callback_class is None:
+        callback_class = getattr(tensorflow.keras.callbacks, layer_type, None)
+    if callback_dict[layer_type] is None:
+        callback_dict[layer_type] = {}
     return callback_class(**callback_dict[layer_type])
 
 def config_callbacks() -> List[tensorflow.keras.callbacks.Callback]:
