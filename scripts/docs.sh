4e4df1d160edc0e8597fc3450f3e13c0ef755a5f
Brian Coltin <brian.coltin@nasa.gov>
2021-03-18 15:05:57 -0700

    Second try to fix gh-pages.

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -4,6 +4,6 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 OUT_DIR=$(readlink -m ${1:-./html})
 cd $SCRIPTPATH/..
-rm -r $OUT_DIR/
+rm -r $OUT_DIR/* # keep .git folder in CI
 pdoc3 --html -c show_type_annotations=True delta --force -o $OUT_DIR
 mv $OUT_DIR/delta/* $OUT_DIR/ && rmdir --ignore-fail-on-non-empty $OUT_DIR/delta
