1fbfea51ba013976af006aaf06f656e55bdfcc7c
Brian Coltin <brian.coltin@nasa.gov>
2021-03-18 14:30:16 -0700

    Fix gh-pages.

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -4,4 +4,6 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 OUT_DIR=$(readlink -m ${1:-./html})
 cd $SCRIPTPATH/..
+rm -r $OUT_DIR/
 pdoc3 --html -c show_type_annotations=True delta --force -o $OUT_DIR
+mv $OUT_DIR/delta/* $OUT_DIR/ && rmdir --ignore-fail-on-non-empty $OUT_DIR/delta
