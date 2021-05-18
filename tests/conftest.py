0bdb2838088c20c3bc3575888d19c1e6482ecc9c
von Pohle, Michael (ARC-TI)[UNIVERSITIES SPACE RESEARCH ASSN] <michael.vonpohle@nasa.gov>
2021-05-17 17:13:39 -0700

    - Added tests and test support code for --prob, --continuous-error, and --continuous-error-abs - Restored the previous change I made to test_classify_main

--- tests/conftest.py
+++ tests/conftest.py
@@ -92,6 +92,24 @@ def original_file():
 
     shutil.rmtree(tmpdir)
 
+# DONE: create for the incremental_config and test_classify_* tests
+#  - create square numpy (10,10) image that contains 0.00-0.99 and label image that is first half 0, second half 1.
+@pytest.fixture(scope="session")
+def incremental_tiff_filenames():
+    tmpdir = tempfile.mkdtemp()
+    image_path = os.path.join(tmpdir, 'image.tiff')
+    label_path = os.path.join(tmpdir, 'label.tiff')
+
+    image = np.reshape(np.arange(0,1,.01), (10,10))
+    # image = np.random.random((128, 128)) #pylint: disable=no-member
+    label = (image >= 0.5).astype(np.uint8)
+    # label = 2 * image
+    tiff.write_tiff(image_path, image)
+    tiff.write_tiff(label_path, label)
+    yield ([image_path], [label_path])
+
+    shutil.rmtree(tmpdir)
+
 @pytest.fixture(scope="session")
 def doubling_tiff_filenames():
     tmpdir = tempfile.mkdtemp()
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -30,6 +30,7 @@ from conftest import config_reset
 from delta.extensions.sources.tiff import TiffImage
 from delta.ml.predict import LabelPredictor, ImagePredictor
 from delta.subcommands.main import main
+from delta.config.extensions import image_writer
 
 @pytest.fixture(scope="session")
 def identity_config(binary_identity_tiff_filenames):
@@ -63,18 +64,133 @@ def identity_config(binary_identity_tiff_filenames):
 
     shutil.rmtree(tmpdir)
 
-def test_predict_main(identity_config, tmp_path):
-    # DONE: add some tests here for prob_error_image?
-    for cmd_option in ["","--prob", "--continuous-error"]:
-        config_reset()
-        model_path = tmp_path / 'model.h5'
-        inputs = tf.keras.layers.Input((32, 32, 2))
-        tf.keras.Model(inputs, inputs).save(model_path)
-        args = 'delta classify --config %s %s %s' % (identity_config, model_path, cmd_option)
-        old = os.getcwd()
-        os.chdir(tmp_path) # put temporary outputs here
-        main(args.split())
-        os.chdir(old)
+# # TODO: create for the test_classify_* tests
+# #   - uses incremental_tiff_filenames
+# #   - add some nodata testing as well?
+# @pytest.fixture(scope="session")
+# def incremental_config(incremental_tiff_filenames):
+#     tmpdir = tempfile.mkdtemp()
+#
+#     config_path = os.path.join(tmpdir, 'dataset.yaml')
+#     with open(config_path, 'w') as f:
+#         f.write('''
+#         dataset:
+#           images:
+#             nodata_value: ~
+#             files:
+#         ''')
+#
+#         for fn in incremental_tiff_filenames[0]:
+#             f.write('      - %s\n' % (fn))
+#         f.write('''
+#           labels:
+#             nodata_value: 2
+#             files:
+#         ''')
+#         for fn in incremental_tiff_filenames[1]:
+#             f.write('      - %s\n' % (fn))
+#         f.write('''
+#           classes: 2
+#         io:
+#           tile_size: [10, 10]
+#         ''')
+#
+#     yield config_path
+#
+#     shutil.rmtree(tmpdir)
+
+def test_classify_main(identity_config, tmp_path):
+    config_reset()
+    model_path = tmp_path / 'model.h5'
+    inputs = tf.keras.layers.Input((32, 32, 2))
+    tf.keras.Model(inputs, inputs).save(model_path)
+    args = 'delta classify --config %s %s' % (identity_config, model_path)
+    old = os.getcwd()
+    os.chdir(tmp_path) # put temporary outputs here
+    main(args.split())
+    os.chdir(old)
+
+# DONE: for testing --prob
+#  - check that output file is continuous  but int-ed
+#  - create square numpy (10,10) image that contains 0.00-0.99
+#  - basic input output match check
+#  - do I need to add nodata to the test?
+def test_predict_prob_output(incremental_tiff_filenames, tmp_path):
+    writer = image_writer('tiff')
+    prob_image_path = str(tmp_path / "test_prob_image.tiff")
+    prob_image = writer(prob_image_path)
+
+    inputs = tf.keras.layers.Input((10, 10, 1))
+    model = tf.keras.Model(inputs, inputs)
+    pred = LabelPredictor(model, prob_image=prob_image)
+    image = TiffImage(incremental_tiff_filenames[0])
+    pred.predict(image)
+
+    prob_image_output = TiffImage(prob_image_path)
+    assert prob_image_output.dtype() == np.uint8
+    prob_array = prob_image_output.read()
+    incremental_image = TiffImage(incremental_tiff_filenames[0])
+    incremental_array = incremental_image.read()
+    np.testing.assert_array_equal(prob_array, np.clip((incremental_array * 254.0).astype(np.uint8), 0, 254) + 1)
+
+# DONE: for testing --continuous-error
+#  - check that output file is continuous  but int-ed
+#  - create square numpy (10,10) image that contains 0.00-0.99 and label image that is first half 0, second half 1.
+#  - expect first half to be positive and last half negative with the right quantities - will be inted of course
+def test_predict_continuous_error_output(incremental_tiff_filenames, tmp_path):
+    writer = image_writer('tiff')
+    continuous_error_image_path = str(tmp_path / "test_continuous_error_image.tiff")
+    continuous_error_image = writer(continuous_error_image_path)
+
+    inputs = tf.keras.layers.Input((10, 10, 1))
+    model = tf.keras.Model(inputs, inputs)
+    pred = LabelPredictor(model, continuous_error_image=continuous_error_image)
+    image = TiffImage(incremental_tiff_filenames[0])
+    label = TiffImage(incremental_tiff_filenames[1])
+    pred.predict(image, label)
+
+    continuous_error_image_output = TiffImage(continuous_error_image_path)
+    assert continuous_error_image_output.dtype() == np.uint8
+    continuous_error_array = continuous_error_image_output.read()
+    incremental_image = TiffImage(incremental_tiff_filenames[0])
+    incremental_array = incremental_image.read()
+    label_image = TiffImage(incremental_tiff_filenames[1])
+    label_array = label_image.read()
+    error_array = incremental_array - label_array
+    # np.clip(((error_array * 127) + 128).astype(np.uint8), 1, 255)
+    error_array_inted = np.clip(((error_array * 127) + 128).astype(np.uint8), 1, 255)
+    # -1 1
+    # * 127 => -127 127
+    # + 128 => 1 255
+    np.testing.assert_array_equal(continuous_error_array, error_array_inted)
+
+
+# DONE: for testing --continuous-error-abs
+#  - check that output file is continuous  but int-ed
+#  - create square numpy (10,10) image that contains 0.00-0.99 and label image that is first half 0, second half 1.
+#  - expect both halves to be positive with right quantities
+def test_predict_continuous_error_abs_output(incremental_tiff_filenames, tmp_path):
+    writer = image_writer('tiff')
+    continuous_error_abs_image_path = str(tmp_path / "test_continuous_error_abs_image.tiff")
+    continuous_error_abs_image = writer(continuous_error_abs_image_path)
+
+    inputs = tf.keras.layers.Input((10, 10, 1))
+    model = tf.keras.Model(inputs, inputs)
+    pred = LabelPredictor(model, continuous_error_abs_image=continuous_error_abs_image)
+    image = TiffImage(incremental_tiff_filenames[0])
+    label = TiffImage(incremental_tiff_filenames[1])
+    pred.predict(image, label)
+
+    continuous_error_abs_image_output = TiffImage(continuous_error_abs_image_path)
+    assert continuous_error_abs_image_output.dtype() == np.uint8
+    continuous_error_abs_array = continuous_error_abs_image_output.read()
+    incremental_image = TiffImage(incremental_tiff_filenames[0])
+    incremental_array = incremental_image.read()
+    label_image = TiffImage(incremental_tiff_filenames[1])
+    label_array = label_image.read()
+    error_abs_array = np.abs(incremental_array - label_array)
+    error_abs_array_inted = np.clip(((error_abs_array * 254) + 1).astype(np.uint8), 1, 255)
+    np.testing.assert_array_equal(continuous_error_abs_array, error_abs_array_inted)
 
 def test_train_main(identity_config, tmp_path):
     config_reset()
