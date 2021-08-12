4c7a81c79a04f312a3361116d6720eea6009e551
Scott <scott.t.mcmichael@nasa.gov>
2021-08-11 17:09:09 -0700

    Improve file checking

--- scripts/classify_directory.py
+++ scripts/classify_directory.py
@@ -10,6 +10,25 @@ import shutil
 import argparse
 
 
+def is_valid_image(image_path):
+    '''Return True if the given path is a valid image on disk'''
+    if (not os.path.exists(image_path)) or (os.path.getsize(image_path) == 0):
+        return False
+    cmd = ['gdalinfo', image_path]
+    result = subprocess.run(cmd, capture_output=True)
+    for line in result.stdout.decode('ascii').split(os.linesep):
+        if 'Size is' in line:
+            parts = line.split()
+            if len(parts) < 4:
+                return False
+            try:
+                i1 = int(parts[2])
+                i2 = int(parts[3])
+                return (i1 > 0) and (i2 > 0)
+            except:
+                return False
+    return False
+
 def main(argsIn):
 
     try:
@@ -45,6 +64,8 @@ def main(argsIn):
         return -1
 
 
+    # TODO: Check for other output files as well!
+
     PREFIX = 'IF_'
     target_paths = []
     for r, d, f in os.walk(args.input_dir):
@@ -66,6 +87,7 @@ def main(argsIn):
     if not target_paths:
         print('No unprocessed input files detected.')
         return 0
+    print('Found ' + str(len(target_paths)) + ' input files to process.')
 
     if not os.path.exists(args.output_dir):
         os.mkdir(args.output_dir)
@@ -74,9 +96,12 @@ def main(argsIn):
     #cmd = ['gdaldem', 'roughness', input_dem_path, roughness_path]
     # TODO: When is this done?
 
-    for (input_path, output_path) in target_paths:
+    for (input_path, delta_output_path) in target_paths:
 
-        output_folder = os.path.dirname(output_path)
+        print('Starting processing for file: ' + input_path)
+        allSucceeded = True
+
+        output_folder = os.path.dirname(delta_output_path)
         # DELTA
         delta_output_folder = os.path.join(output_folder, 'delta')
         cmd = ['delta', 'classify', '--image', input_path,
@@ -86,10 +111,14 @@ def main(argsIn):
             cmd += ['--config', args.delta_config]
         result = subprocess.run(cmd, capture_output=True)
         #for line in result.stdout.decode('ascii').split(os.linesep):
+        if not is_valid_image(delta_output_path):
+            print('delta processing FAILED!')
+            allSucceeded = False
 
 
         # presoak
         presoak_output_folder = os.path.join(output_folder, 'presoak')
+        presoak_output_path = os.path.join(presoak_output_folder, 'merged_cost.tif')
         cmd = ['presoak', '--max-cost', '20',
                '--elevation', input_dem_path,
                '--flow', os.path.join(args.fist_data_dir, 'p.vrtd/arcgis.vrt'),
@@ -97,17 +126,31 @@ def main(argsIn):
                '--image', input_path,  '--output_dir', presoak_output_folder]
         cmd += unknown_args
         result = subprocess.run(cmd, capture_output=True)
+        if not is_valid_image(presoak_output_path):
+            print('presoak processing FAILED!')
+            allSucceeded = False
 
         #HMTFIST
         hmtfist_work_folder = os.path.join(output_folder, 'hmtfist_work')
         hmtfist_output_folder = os.path.join(output_folder, 'hmtfist')
-        cmd = ['HMTFIST_caller',#  '--exe-path', TODO,
-               '--work-dir', hmtfist_work_folder, #'--delete-workdir',
-               '--presoak-dir', presoak_output_folder,
-               '--delta-prediction-path', output_path,
-               '--pits-path', TODO,
-               '--roughness-path', TODO,
-               '--canopy-path', TODO,
-               '--parameter-path', args.hmt_params,
-               '--output-dir', hmtfist_output_folder]
-        result = subprocess.run(cmd, capture_output=True)
+        hmtfist_output_path = os.path.join(hmtfist_output_folder, 'output_prediction.tif')
+        if is_valid_image(delta_output_path) and is_valid_image(presoak_output_path):
+            cmd = ['HMTFIST_caller',#  '--exe-path', TODO,
+                '--work-dir', hmtfist_work_folder, #'--delete-workdir',
+                '--presoak-dir', presoak_output_folder,
+                '--delta-prediction-path', delta_output_path,
+                '--pits-path', TODO,
+                '--roughness-path', TODO,
+                '--canopy-path', TODO,
+                '--parameter-path', args.hmt_params,
+                '--output-dir', hmtfist_output_folder]
+            result = subprocess.run(cmd, capture_output=True)
+
+        if is_valid_image(hmtfist_output_path):
+            print('hmtfist processing FAILED!')
+            allSucceeded = False
+
+        if allSucceeded:
+            print('Completed processing file: ' + input_path)
+        else:
+            print('Unable to complete processing for file: ' + input_path)
