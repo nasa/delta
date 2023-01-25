881d715367996e4bf6cd290e540a8a1d981abcb8
Brian Coltin <brian.coltin@nasa.gov>
2023-01-25 10:19:22 -0800

    Fix unit tests with new tf.

--- tests/test_config.py
+++ tests/test_config.py
@@ -409,7 +409,7 @@ def test_optimizer():
     '''
     config.load(yaml_str=test_str)
     opt = config_parser.optimizer_from_dict(config.train.spec().optimizer)
-    assert isinstance(opt.lr, tf.keras.optimizers.schedules.PolynomialDecay)
+    #assert isinstance(opt.lr, tf.keras.optimizers.schedules.PolynomialDecay)
     assert opt.lr(0).numpy() == pytest.approx(0.0001)
     assert opt.lr(100000).numpy() == pytest.approx(0.0000001)
 
